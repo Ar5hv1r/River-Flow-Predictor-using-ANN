51b3605808e8311eb9d27d7cad296b9c6695ad7c
Ar5hv1r <arshvirt2@gmail.com>
2022-03-13 13:19:06 +0000

    read in data and inital graph

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pandas as pd
-import pprint
+import matplotlib.pyplot as plt
 
-printer = pprint.PrettyPrinter()
+dataframe = pd.read_excel("Ouse93-96 - Student.xlsx", sheet_name="Clean Data")
+dates = dataframe["Dates"]
+predictor1 = dataframe["Crakehill"]  # all values in column
+predictor1_training = dataframe["Crakehill"][:732]  # 2 years of training data
+predictor1_validation = dataframe["Crakehill"][732:976]  # 1 year of validation data
+predictor1_testing = dataframe["Crakehill"][976:]  # testing data
 
-workbook = "Ouse93-96 - Student.xlsx"
-sheet = pd.read_excel(workbook, sheet_name="1993-96")
-
-crakehill = sheet["Mean Daily Flow - Cumecs"]
-printer.pprint(crakehill)
+plt.plot(dates, predictor1)
+plt.ylabel("Mean Daily Flow - Cumecs")
+plt.xlabel("Date")
+plt.show()
